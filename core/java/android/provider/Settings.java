70c8b2a22692b365321ac3761d4c74b2a9fea4c3
cjybyjk <cjybyjk@zjnu.edu.cn>
2020-10-06 10:18:40 +0800

    base: add StatusBar Lyric [1/2]
    Signed-off-by: cjybyjk <cjybyjk@zjnu.edu.cn>
    Change-Id: I3d712534ed0d55985d85b25262feccf2f5f23cbd
    Signed-off-by: chaptsand <chaptsand@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5273,6 +5273,12 @@ public final class Settings {
          */
 
         /**
+         * Whether to show the lyric on the status bar
+         * @hide
+         */
+        public static final String STATUS_BAR_SHOW_LYRIC = "status_bar_show_lyric";
+
+        /**
          * Whether to show the notification ticker on the status bar
          * @hide
          */
