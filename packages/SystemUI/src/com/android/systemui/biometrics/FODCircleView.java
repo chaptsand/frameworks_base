01d246fcbfa478a3da69be544f9a80553f8311c1
dataoutputstream <mb150996@gmail.com>
2021-06-19 21:44:06 +0800

    umi: fod fix
    Signed-off-by: chaptsand <chaptsand@gmail.com>
    Change-Id: I80487d212740e9eccd928214fcfcfc1f32e1a333

--- packages/SystemUI/src/com/android/systemui/biometrics/FODCircleView.java
+++ packages/SystemUI/src/com/android/systemui/biometrics/FODCircleView.java
@@ -344,6 +344,7 @@ public class FODCircleView extends ImageView implements ConfigurationListener {
         mParams.type = WindowManager.LayoutParams.TYPE_DISPLAY_OVERLAY;
         mParams.flags = WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE |
                 WindowManager.LayoutParams.FLAG_LAYOUT_IN_SCREEN |
+                WindowManager.LayoutParams.FLAG_DIM_BEHIND |
                 WindowManager.LayoutParams.FLAG_WATCH_OUTSIDE_TOUCH;
         mParams.gravity = Gravity.TOP | Gravity.LEFT;
 
@@ -354,6 +355,7 @@ public class FODCircleView extends ImageView implements ConfigurationListener {
         mParams.setTitle("Fingerprint on display");
         mPressedParams.setTitle("Fingerprint on display.touched");
 
+        mParams.dimAmount = 0.0f;
         mPressedView = new ImageView(context)  {
             @Override
             protected void onDraw(Canvas canvas) {
